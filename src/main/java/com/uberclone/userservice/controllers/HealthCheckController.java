599076eb3e004f2496fc138226161731eee3e16f
Nehul Makani <makaninehul@gmail.com>
2023-10-25 21:26:10 +0530

    build health check api

--- src/main/java/com/uberclone/userservice/controllers/HealthCheckController.java
+++ src/main/java/com/uberclone/userservice/controllers/HealthCheckController.java
@@ -1,2 +1,20 @@
-package com.uberclone.userservice.controllers;public class HealthCheckController {
+package com.uberclone.userservice.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> getHealthCheckResponse(){
+        try{
+            return new ResponseEntity<>("User Service is up and running", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("User Service is down", HttpStatus.GATEWAY_TIMEOUT);
+        }
+    }
 }
